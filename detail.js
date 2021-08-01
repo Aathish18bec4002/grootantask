cdfccb3ddb0a4dc29385c986139b0f3713ae149f
Aathish18bec4002 <88273888+Aathish18bec4002@users.noreply.github.com>
2021-08-01 16:58:12 +0530

    Add files via upload

---
+++ detail.js
@@ -0,0 +1,12 @@
+       //form 1
+
+        function vfun3()
+{
+        
+          window.location.href="AATHISH.html";           
+}  
+function vfun4()
+{
+        
+          window.location.href="ABISHEK.html";           
+}  
---
+++ main2.js
@@ -0,0 +1,64 @@
+       //form 1
+
+        function vfun(){
+            var uname=document.forms["myform"]["uname"].value;
+            var upswd=document.forms["myform"]["upswd"].value;
+        if(uname==null || uname=="" ){
+                  document.getElementById("errorBox").innerHTML =
+                   "enter the user name";
+                 return false;
+        }
+
+        if(upswd==null || upswd==""){
+                  document.getElementById("errorBox").innerHTML =
+                   "enter the password";
+                 return false;
+        }
+
+        if(uname != '' && upswd != '' )
+{
+          alert("Login successfully");
+          window.location.href="form.html";           
+}  
+
+        }
+
+ //form 2
+
+  function vfun1(){
+            var uname1=document.forms["myform2"]["uname1"].value;
+            var upswd1=document.forms["myform2"]["upswd1"].value;
+            var upswd2=document.forms["myform2"]["upswd2"].value;
+
+
+        if(uname1==null || uname1=="" ){
+                  document.getElementById("errorBox").innerHTML =
+                   "enter the user name";
+                 return false;
+        }
+        if(upswd1==null || upswd1=="" ){
+                  document.getElementById("errorBox").innerHTML =
+                   "enter the password";
+                 return false;
+        }
+
+        if(upswd2==null || upswd2==""){
+                  document.getElementById("errorBox").innerHTML =
+                   "enter the password";
+                 return false;}
+
+ 		if(upswd1 != upswd2){
+                  document.getElementById("errorBox").innerHTML =
+                   "password dont match";
+                 return false;}
+
+        
+
+        if (uname1 != '' && upswd1 != '' && upswd2 != '' && upswd1 == upswd2)
+
+{
+          alert("Registation successfully");
+          window.location.href="index.html";
+
+        }
+}
